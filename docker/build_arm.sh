2adfa405eec9790d1cfc22ceb727bfee025435c6
Greg Soltis <greg@goldfiglabs.com>
2021-06-25 11:27:22 -0700

    Upgrade psycopg2-binary to a version with arm64 packages prebuilt. Revert a bunch of build hacks as a result

--- docker/build_arm.sh
+++ docker/build_arm.sh
@@ -5,23 +5,9 @@ set -e
 PACKAGE=$1
 VERSION=$2
 
-export DOCKER_BUILDKIT=1
-WHEEL_TAG=goldfig/tempbuildwheel:latest
-docker build --platform linux/arm64 -f docker/BuildWheelDockerFile -t $WHEEL_TAG .
-
-CONTAINER_ID=$(docker run -t -d --rm $WHEEL_TAG)
-echo "Container ID $CONTAINER_ID"
-WHEEL_FILE=$(docker exec $CONTAINER_ID /usr/local/bin/pip cache list --format abspath)
-echo "Wheel file $WHEEL_FILE"
-LOCAL_WHEEL=psycopg2_binary-2.8.6-cp39-cp39-linux_aarch64.whl
-docker cp $CONTAINER_ID:$WHEEL_FILE $LOCAL_WHEEL
-docker stop $CONTAINER_ID
-
 echo "Building package ${PACKAGE}"
 
-pipenv lock -r > requirements.tmp
-sed '/psycopg2-binary.*/d' requirements.tmp > requirements.txt
-rm requirements.tmp
+pipenv lock -r > requirements.txt
 
 INTROSPECTOR_DOCKER_REPO=${DOCKER_REPO:-goldfig}
 IMAGE="${INTROSPECTOR_DOCKER_REPO}/${PACKAGE}:arm64-${VERSION}"
