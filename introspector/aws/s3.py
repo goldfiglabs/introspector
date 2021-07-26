e1d8783df0ed2ee90506d47fcef6cef0d27cca11
Greg Soltis <greg@goldfiglabs.com>
2021-07-26 09:43:01 -0700

    Handle s3 buckets that are missing features due to location constraints

--- introspector/aws/s3.py
+++ introspector/aws/s3.py
@@ -2,6 +2,8 @@ import json
 import logging
 from typing import Any, Dict, Iterator, Tuple
 
+import botocore
+
 from introspector.aws.fetch import ServiceProxy
 from introspector.aws.svc import GlobalService, ServiceSpec, resource_gate
 
@@ -14,12 +16,26 @@ def _import_bucket(proxy: ServiceProxy, bucket_metadata) -> Dict:
   for op_name in proxy.resource_names():
     canonical_name = proxy.canonical_name(op_name)
     if op_name.startswith('list_'):
-      list_result = proxy.list(op_name, Bucket=bucket_metadata['Name'])
+      try:
+        list_result = proxy.list(op_name, Bucket=bucket_metadata['Name'])
+      except botocore.exceptions.ClientError as e:
+        error = e.response.get('Error', {}).get('Code', '')
+        if error == 'IllegalLocationConstraintException':
+          list_result = None
+        else:
+          raise e
       if list_result is not None:
         key = canonical_name[len('ListBucket'):]
         result[key] = list_result[1]
       continue
-    attr_result = proxy.get(op_name, Bucket=bucket_metadata['Name'])
+    try:
+      attr_result = proxy.get(op_name, Bucket=bucket_metadata['Name'])
+    except botocore.exceptions.ClientError as e:
+      error = e.response.get('Error', {}).get('Code', '')
+      if error == 'IllegalLocationConstraintException':
+        attr_result = None
+      else:
+        raise e
     if attr_result is not None:
       key = canonical_name[len('GetBucket'):]
       # TODO: enumerate these methods
@@ -31,8 +47,15 @@ def _import_bucket(proxy: ServiceProxy, bucket_metadata) -> Dict:
         result[key] = json.loads(result[key])
     elif op_name == 'get_bucket_location':
       result['Location'] = {'LocationConstraint': 'us-east-1'}
-  public_conf_resp = proxy.get('get_public_access_block',
-                               Bucket=bucket_metadata['Name'])
+  try:
+    public_conf_resp = proxy.get('get_public_access_block',
+                                Bucket=bucket_metadata['Name'])
+  except botocore.exceptions.ClientError as e:
+    error = e.response.get('Error', {}).get('Code', '')
+    if error == 'IllegalLocationConstraintException':
+      public_conf_resp = None
+    else:
+      raise e
   if public_conf_resp is not None:
     public_attrs = public_conf_resp['PublicAccessBlockConfiguration']
     result.update(public_attrs)
